c77cf9c7864c3ad2902ff611be61915ccfe792d8
Netia Ingram <netia1128@hotmail.com>
2021-03-29 20:47:01 -0600

    Check that ship has name and length

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -9,5 +9,11 @@ RSpec.describe Ship do
     it 'creates an instance of a ship' do
       expect(@ship).to be_an_instance_of(Ship)
     end
+    it 'has a name' do
+      expect(@ship.name).to eq("Cruiser")
+    end
+    it 'has a length' do
+      expect(@ship.length).to eq(3)
+    end
   end
 end
