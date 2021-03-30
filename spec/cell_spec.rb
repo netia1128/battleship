cbe4f4a49e069b0f669983d96f54639e8aab8d99
Brant Fuller <brant.fuller@gmail.com>
2021-03-30 14:38:23 -0500

    Test cell has a coordinate

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -10,5 +10,9 @@ RSpec.describe Cell do
     it 'creates an instance of a cell' do
       expect(@cell).to be_an_instance_of(Cell)
     end
+
+    it 'has a coordinate' do
+      expect(@cell.coordinate).to eq("B4")
+    end
   end
 end
