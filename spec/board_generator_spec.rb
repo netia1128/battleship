ef62e89ccadfec4b8ca7df5a04f0c38bf81f038c
Netia Ingram <netia1128@hotmail.com>
2021-04-02 17:09:19 -0600

    Add test for #initialize creates an empty board_array

--- spec/board_generator_spec.rb
+++ spec/board_generator_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe BoardGenerator do
       board_generator = BoardGenerator.new(4)
       expect(board_generator).to be_an_instance_of(BoardGenerator)
     end
+    it 'has an empty board array by default' do
+      board_generator = BoardGenerator.new(7)
+      expect(board_generator.board_array).to eq([])
+    end
   end
 end
