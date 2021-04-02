8dc8b150b835bc96f804b4652825a84a93bc34aa
Brant Fuller <brant.fuller@gmail.com>
2021-04-02 12:25:23 -0500

    Refactor #valid_placement and #render

--- lib/board.rb
+++ lib/board.rb
@@ -24,23 +24,19 @@ class Board
   end
 
   def valid_placement?(ship, coordinates)
-    if coordinates_match_ship_length?(coordinates, ship) ||
-       no_duplicate_coordinates?(coordinates) ||
-       coordinates_not_empty?(coordinates) ||
-       is_not_consecutive?(coordinates, ship)
-      return false
-    else
-      return true
-    end
+    coordinates_match_ship_length?(coordinates, ship) &&
+    no_duplicate_coordinates?(coordinates) &&
+    coordinates_empty?(coordinates) &&
+    is_consecutive?(coordinates, ship)
   end
 
-  def is_not_consecutive?(coordinates, ship)
+  def is_consecutive?(coordinates, ship)
     if is_horizontal?(coordinates)
-      return !((user_coordinate_numbers(coordinates).last - user_coordinate_numbers(coordinates).first) + 1 == ship.length)
+      return ((user_coordinate_numbers(coordinates).last - user_coordinate_numbers(coordinates).first) + 1 == ship.length)
     elsif is_vertical?(coordinates)
-      return !((user_coordinate_letters(coordinates).last.ord - user_coordinate_letters(coordinates).first.ord) + 1 == ship.length)
+      return ((user_coordinate_letters(coordinates).last.ord - user_coordinate_letters(coordinates).first.ord) + 1 == ship.length)
     else
-      return true
+      return false
     end
   end
 
@@ -48,24 +44,18 @@ class Board
     @cells.keys.to_a.include? coordinate.to_sym
   end
 
-  def coordinates_not_empty?(coordinates)
+  def coordinates_empty?(coordinates)
     coordinates.each do |coordinate|
-      if @cells[coordinate.to_sym].empty?
-        return false
-      end
+      @cells[coordinate.to_sym].empty?
     end
   end
 
   def coordinates_match_ship_length?(coordinates, ship)
-    if coordinates.count != ship.length
-      return false
-    end
+    coordinates.count == ship.length
   end
 
   def no_duplicate_coordinates?(coordinates)
-    if coordinates.uniq.count != coordinates.length
-      return false
-    end
+    coordinates.uniq.count == coordinates.length
   end
 
   def split_user_coordinates(coordinates)
@@ -108,30 +98,23 @@ class Board
       # exit
       return false
     end
-    # want to change each coordinate's ::ship
-    # maybe using the cell's place_ship method?
-    # This would be to update the status to "S"
     coordinates.each do |coordinate|
       @cells[coordinate.to_sym].place_ship(ship)
     end
   end
 
   def render(show_ships = false)
-    count = 0
-    array = []
-    string = ""
-    @cells.each do |key, cell|
-      array << cell.render(show_ships)
-    end
-    board_dimension.times do
-      string += "#{@cells.keys[count].to_s[0]} "
-      board_dimension.times do
-        string += "#{array[count]} "
-        count += 1
+    string = top_row
+    @cells.each do |key, value|
+      if key.to_s[1] == "1"
+        string += "#{key.to_s[0]} #{value.render(show_ships)} "
+      elsif key.to_s[1].to_i == board_dimension
+        string += "#{value.render(show_ships)} \n"
+      else
+       string += "#{value.render(show_ships)} "
       end
-      string += "\n"
     end
-    top_row + string
+    string
   end
 
   def top_row
@@ -145,16 +128,4 @@ class Board
   def board_numbers
     (1..board_dimension).to_a
   end
-
-  # def fire_upon(coordinate)
-  #   @cells[coordinate.to_sym].fire_upon
-  #   if !@cells[coordinate.to_sym].ship.nil?
-  #     @cells.values.each do |cell|
-  #       if cell.ship == @cells[coordinate.to_sym].ship
-  #         puts @cells[coordinate.to_sym]
-  #       end
-  #     end
-  #   end
-  # end
-
 end
