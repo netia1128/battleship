8bd7cbc860500e579dfb61a9e984d2aa694fefde
Netia Ingram <netia1128@hotmail.com>
2021-04-02 17:25:08 -0600

    Add test for #make_board_array

--- spec/board_generator_spec.rb
+++ spec/board_generator_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe BoardGenerator do
       expect(board_generator.board_hash).to eq({})
     end
   end
+  # describe '#make_board_hash' do
+  #   it 'creates a hash of the generated board' do
+  #     board_generator = BoardGenerator.new(7)
+  #     board_generator.board_hash
+  #     expect(board_generator.board_hash).to eq({})
+  #   end
+  # end
+  describe '#make_board_array' do
+    it 'creates an array of the generated board' do
+      board_generator = BoardGenerator.new(3)
+      board_generator.make_board_array
+      expect(board_generator.board_array).to eq(["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"])
+    end
+  end
 end
