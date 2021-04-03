c62a8429fe5f55661ef817830127ce4fcd42c08b
Brant Fuller <brant.fuller@gmail.com>
2021-04-02 20:52:35 -0500

    Create #player_information method

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,5 @@
+require_relative 'player'
+
 class Game
 
   def main_menu_statement
@@ -5,7 +7,7 @@ class Game
     puts "Enter P to play or Q to quit"
     input = gets.chomp.upcase
     if input == "P"
-      puts "start_game"
+      player_information
     elsif input == "Q"
       quit_game
     else
@@ -17,8 +19,28 @@ class Game
     puts "Thanks for playing"
   end
 
-  def start_game
-    gets.chomp
+
+
+  def player_information
+    puts "*******************"
+    puts "What is your name?"
+    name = gets.chomp
+    puts "Hi #{name}. " +
+    "My name is Computron. I will be your computer opponent."
+    puts "To start, we will create a square board to play with.\n" +
+    "Your board can be anywhere between 4 and 9 cells wide.\n" +
+    "How many cells would you like in each row?"
+    board_dimension = gets.chomp.to_i
+    if (4..9).to_a.include? board_dimension
+      puts "Great! we will start with a #{board_dimension}" +
+      " by #{board_dimension} board."
+      player = Player.new(name, board_dimension)
+      # computron = Player.new("Computron", board_dimension)
+
+    else
+      puts "try again"
+    end
+
   end
 
 end
--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,16 @@
+require_relative 'ship'
+require_relative 'board_generator'
+
 class Player
 
   def initialize(name, board_dimension)
     @name = name
-    @board = Board.new(
+    @board = BoardGenerator.new(board_dimension)
 
     @crusier = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
     @tug_boat = Ship.new("Tug Boat", 1)
+
   end
 
 end
