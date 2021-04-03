9388f22747b72b9d614a9310a967df023a526a6a
Brant Fuller <brant.fuller@gmail.com>
2021-04-02 21:08:09 -0500

    Finish #player_information method

--- lib/game.rb
+++ lib/game.rb
@@ -31,16 +31,15 @@ class Game
     "Your board can be anywhere between 4 and 9 cells wide.\n" +
     "How many cells would you like in each row?"
     board_dimension = gets.chomp.to_i
-    if (4..9).to_a.include? board_dimension
-      puts "Great! we will start with a #{board_dimension}" +
-      " by #{board_dimension} board."
-      player = Player.new(name, board_dimension)
-      # computron = Player.new("Computron", board_dimension)
-
-    else
-      puts "try again"
+    until ((4..9).to_a.include? board_dimension)
+      puts "Sorry #{name} that is not a valid board size.\n" +
+      "Please choose a board size between 4 and 9 cells wide."
+      board_dimension = gets.chomp.to_i
     end
-
+    puts "Great! we will start with a #{board_dimension}" +
+    " by #{board_dimension} board."
+    player = Player.new(name, board_dimension)
+    computron = Player.new("Computron", board_dimension)
   end
 
 end
